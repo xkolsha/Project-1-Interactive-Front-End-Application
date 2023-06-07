b3710fc1aa909abf1d2d44a61a3496258f1c230e
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-06-06 21:00:23 -0300

    added call function for the unsplash api in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-fetch('https://api.unsplash.com/search/photos?query=${keyword}&per_page=9'
-    headers: {
-        Authorization: 'tZgxG3ifL1I1t2iKVY7Pm9kjOxi7M06Ix8c29PJOxfM'
-    }
-);
+unSplashKey = 'tZgxG3ifL1I1t2iKVY7Pm9kjOxi7M06Ix8c29PJOxfM'
+unsplashUrl = 'https://api.unsplash.com/photos/random?client_id=';
+
+
+// call the unSplash API to fetch an image from a URL
+fetch(unsplashUrl + unSplashKey)
+    .then(response => response.json());
+    .then(data => {
+        var imageLink = data.urls.regular;
+        var image = new Image();
+        image.crossOrigin = '';
+        image.src = imageLink;
+
+        image.onload = function() {
+            var colorPalette = generatePallet(image);
+
+            displayPallet(colorPalette);
+
+        };
+    });
