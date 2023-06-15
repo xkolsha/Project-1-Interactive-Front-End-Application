72078b7de2b859a8a5b59e6527074dfd65dcd893
xkolsha <aviad85@outlook.com>
2023-06-14 22:35:09 -0300

    added footer.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,6 +142,9 @@ document.getElementById("generate-btn").addEventListener("click", function () {
   }
 });
 
+fetch("./footer.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("footer").innerHTML = data));
 // End of Aviad Code
 
 // Start of Chris Code:
